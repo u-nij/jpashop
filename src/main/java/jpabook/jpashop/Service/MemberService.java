6b2c5de4ff6c7a972373a1f82f238e5215fb6cd9
u-nij <u.nij0509@gmail.com>
2022-08-29 17:10:31 +0900

    회원 서비스 개발

---
+++ src/main/java/jpabook/jpashop/Service/MemberService.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.Domain.Member;
+import jpabook.jpashop.Repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor // final이 있는 필드만 사용해 생성자를 만들어준다.
+public class MemberService {
+
+    private MemberRepository memberRepository;
+    // 컴파일 시점에 생성자를 확인할 수 있기 때문에 final로 설정하는 것을 권장
+
+    // 회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    // 회원 단건 조회
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+}
