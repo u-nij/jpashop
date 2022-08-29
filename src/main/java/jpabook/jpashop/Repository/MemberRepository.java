e929fb7ffb24ac1bd9f6197fbe918cdffa4a6ba7
u-nij <u.nij0509@gmail.com>
2022-08-29 17:09:11 +0900

    회원 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/Repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.Repository;
+
+import jpabook.jpashop.Domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                // JPQL: 기능적으로는 동일. SQL과 다르게, 엔티티 객체를 대상으로 쿼리.
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
